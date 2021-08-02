358135812bf527062057efda76f401f8726f2e3e
Leen Hazaimeh <leenhazaimeh94@gmail.com>
2021-08-02 18:36:00 +0300

    done

--- python/code_challenges/linked_list/linked_list.py
+++ python/code_challenges/linked_list/linked_list.py
@@ -10,21 +10,18 @@ class Node:
 
 
 class LinkedList:
-  
-    def __init__(self):
-  
-
-    def insert(self, data=None):
+  def __init__(self):
+      def insert(self, data=None):
 
         new_node = Node(data)
        
         if self.head:
-            new_node.next = self.head  # set our current pointer to the head
+            new_node.next = self.head 
            
         self.head = new_node
     
 
-    def includes(self, value):
+def includes(self, value):
 
         current = self.head
         is_include = False
@@ -34,7 +31,7 @@ class LinkedList:
             current = current.next
         return is_include
 
-    def append(self, data):
+def append(self, data):
 
         new_data = Node(data)
         current = self.head
@@ -45,7 +42,7 @@ class LinkedList:
         else:
             self.head = new_data
 
-    def insertAfter(self, data, new_data):
+def insertAfter(self, data, new_data):
 
         try:
           if self.includes(data):
@@ -60,7 +57,7 @@ class LinkedList:
         except:
           raise Exception("the value not found in the list")
 
-    def insertBefore(self, data, new_data):
+def insertBefore(self, data, new_data):
 
         try:
             if self.includes(data):
@@ -79,7 +76,26 @@ class LinkedList:
         except:
           raise Exception("the value not found in the list")
 
-    def __str__(self):
+def kthFromEnd(self, k: int):
+
+        length = self.length()
+
+        if k >= length or k < 0:
+            return 'IndexError'
+        current = self.head
+        for i in range(length-k):
+            if i == length-k-1:
+                return current.data
+            current = current.next
+
+def length(self):
+        length = 0
+        current = self.head
+        while(current):
+            length += 1
+            current = current.next
+        return length
+def __str__(self):
  
        
         list_data = ""
@@ -95,11 +111,17 @@ class LinkedList:
         return list_data
 
         if __name__ == "__main__":
-    linked = LinkedList()
-    linked.append(1)
-    linked.append(3)
-    linked.append(2)
-    linked.insertAfter(3, 10)
-    linked.insertBefore(100, 7)
-    linked.insertAfter(25,20)
-    print(linked)
+            
+
+
+         linked = LinkedList()
+
+
+
+linked.append(1)
+linked.append(3)
+linked.append(2)
+linked.insertAfter(3, 10)
+linked.insertBefore(100, 7)
+linked.insertAfter(25,20)
+print(linked)
