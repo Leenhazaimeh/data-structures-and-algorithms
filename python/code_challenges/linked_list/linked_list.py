fa8d74bb777f595f2da4ca163d4578d79cf8ec36
Leen Hazaimeh <leenhazaimeh94@gmail.com>
2021-08-12 12:37:00 +0300

    done

---
+++ python/code_challenges/linked_list/linked_list.py
@@ -0,0 +1,93 @@
+class Node:
+    def __init__(self, value=""):
+        self.value = value
+        self.next = None
+
+
+class LinkedList():
+
+    def __init__(self):
+        self.head = None
+        self.values=[]
+
+    def insert(self, value):
+        self.values.append(value)
+        node = Node(value)
+
+        if self.head:
+            node.next = self.head
+
+        self.head = node
+
+
+    def includes(self,value):
+        current=self.head
+        while current:
+            if current.value==value:
+                return True
+            else:
+                current=current.next
+        return False
+
+
+    def __str__(self):
+        string = ""
+        current = self.head
+
+        while current:
+            string += f"{str(current.value)} -> "
+            current = current.next
+        string += "NULL"
+        return string
+
+    def appendvalue(self,value):
+        new_node=Node(value)
+        if self.head==None:
+            self.head=new_node
+        else:
+            currunt=self.head
+            while currunt.next:
+                currunt=currunt.next
+            currunt.next=new_node
+        
+       
+                
+    def insert_befor(self,value,new_value):
+        new_node=Node(new_value)
+        if self.head.value==value:
+            self.insert(new_value)
+        else:
+            currunt=self.head
+            while currunt.next:
+                if currunt.next.value==value:
+                    new_node.next=currunt.next
+                    currunt.next=new_node
+                    break
+                currunt=currunt.next
+
+    def insert_after(self,value,new_value):
+        new_node=Node(new_value)
+        if self.head.value==value:
+            self.head.next=new_node
+        else:
+            currunt=self.head
+            while currunt:
+                if currunt.value==value:
+                    new_node.next=currunt.next
+                    currunt.next=new_node
+                    break
+                currunt=currunt.next   
+
+
+if __name__ == "__main__":
+  ll = LinkedList()
+  ll.insert(5)
+  ll.insert(4)
+  print(ll)
+  node1 =  Node(1)
+  node2 =  Node(2)
+  node3 = node1 + node2
+  print(node3)
+
+  for value in ll:
+    print(value)
---
+++ python/code_challenges/linked_list/test_linked_list.py
@@ -0,0 +1,153 @@
+from linked_list import Node,LinkedList
+import pytest
+
+
+
+
+# Can successfully instantiate an empty linked list
+def test_linkedlist():
+    assert LinkedList()
+
+
+# Can properly insert into the linked list
+def test_insert():
+  # arrange
+    ll = LinkedList()
+    with pytest.raises(AttributeError):
+        ll.head.value
+    ll.insert(5)
+    actual = ll.head.value
+    assert actual == 5
+
+#The head property will properly point to the first node in the linked list
+def test_if_head_is_head():
+    test=LinkedList()
+    test.insert(66)
+    test.insert(55)
+    test.insert(45)
+    test.insert(45)
+
+    current=test.head
+    current=current.next
+    while current:
+        if current.next==test.head:
+            assert False
+        else:
+            current=current.next
+    return False
+
+
+
+
+
+#Can properly insert multiple nodes into the linked list
+def test_inserting_more():
+    expected="45 -> 55 -> 66 -> NULL"
+    test=LinkedList()
+    test.insert(66)
+    test.insert(55)
+    test.insert(45)
+    actual=test.__str__()
+    assert actual==expected
+
+# Will return true when finding a value within the linked list that exists
+def test_true_includes():
+    test=LinkedList()
+    test.insert(66)
+    test.insert(55)
+    test.insert(45)
+    actual=test.includes(55)
+    assert actual==True
+
+#Will return false when searching for a value in the linked list that does not exist
+def test_false_includes():
+    test=LinkedList()
+    test.insert(66)
+    test.insert(55)
+    test.insert(45)
+    actual=test.includes(555)
+    assert actual==False
+
+
+# Can properly return a collection of all the values that exist in the linked list
+def test_all_values_property():
+    test=LinkedList()
+    test.insert(66)
+    test.insert(55)
+    test.insert(45)
+    if ((66 in test.values) and(55 in test.values) and(45 in test.values)):
+        assert True
+    else:
+        assert False
+
+# Can successfully add a node to the end of the linked list
+def test_append():
+    test=LinkedList()
+    test.insert(66)
+    test.insert(55)
+    test.insert(45)
+    test.appendvalue(77)
+    expected="45 -> 55 -> 66 -> 77 -> NULL"
+    actual=test.__str__()
+    assert actual==expected
+
+# Can successfully add multiple nodes to the end of a linked list
+def test_multi_append():
+    test=LinkedList()
+    test.insert(66)
+    test.insert(55)
+    test.insert(45)
+    test.appendvalue(77)
+    test.appendvalue(88)
+    expected="45 -> 55 -> 66 -> 77 -> 88 -> NULL"
+    actual=test.__str__()
+    assert actual==expected
+
+# Can successfully insert a node before a node located i the middle of a linked list
+def test_insert_befor():
+    test=LinkedList()
+    test.insert(66)
+    test.insert(55)
+    test.insert(45)
+    test.insert_befor(55,50)
+    expected="45 -> 50 -> 55 -> 66 -> NULL"
+    actual=test.__str__()
+    assert actual==expected
+
+#Can successfully insert a node before the first node of a linked list
+
+def test_first_insert():
+    test=LinkedList()
+    test.insert(66)
+    test.insert(55)
+    test.insert(45)
+    test.insert_befor(45,50)
+    expected="50 -> 45 -> 55 -> 66 -> NULL"
+    actual=test.__str__()
+    assert actual==expected
+
+#Can successfully insert after a node in the middle of the linked list
+def test_insert_after():
+    test=LinkedList()
+    test.insert(66)
+    test.insert(55)
+    test.insert(45)
+    test.insert_after(55,50)
+    expected="45 -> 55 -> 50 -> 66 -> NULL"
+    actual=test.__str__()
+    assert actual==expected
+
+
+#Can successfully insert a node after the last node of the linked list
+def test_insert_after_last():
+    test=LinkedList()
+    test.insert(66)
+    test.insert(55)
+    test.insert(45)
+    test.insert_after(66,50)
+    expected="45 -> 55 -> 66 -> 50 -> NULL"
+    actual=test.__str__()
+    assert actual==expected
+
+
+
