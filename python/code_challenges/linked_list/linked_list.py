7c799b5a781416d124c7ed5341f82485c1d43b8b
Leen Hazaimeh <leenhazaimeh94@gmail.com>
2021-08-12 14:10:13 +0300

    fix imports error

--- python/code_challenges/linked_list/linked_list.py
+++ python/code_challenges/linked_list/linked_list.py
@@ -1,129 +1,99 @@
 class Node:
-    def __init__(self, data=None):
-        self.data = data
+    def __init__(self, value=""):
+        self.value = value
         self.next = None
-    
+
+
+class LinkedList():
+
+    def __init__(self):
+        self.head = None
+        self.values=[]
+
+    def insert(self, value):
+        self.values.append(value)
+        node = Node(value)
+
+        if self.head:
+            node.next = self.head
+
+        self.head = node
+
+
+    def includes(self,value):
+        current=self.head
+        while current:
+            if current.value==value:
+                return True
+            else:
+                current=current.next
+        return False
+
 
     def __str__(self):
-        return f"{self.data}"
-
-
-
-class LinkedList:
-  def __init__(self):
-      def insert(self, data=None):
-
-        new_node = Node(data)
-       
-        if self.head:
-            new_node.next = self.head 
-           
-        self.head = new_node
-    
-
-def includes(self, value):
-
+        string = ""
         current = self.head
-        is_include = False
-        while (current):
-            if current.data == value:
-                is_include = True
-            current = current.next
-        return is_include
 
-def append(self, data):
-
-        new_data = Node(data)
-        current = self.head
-        if self.head:
-            while(current.next):
-                current = current.next
-            current.next = new_data
-        else:
-            self.head = new_data
-
-def insertAfter(self, data, new_data):
-
-        try:
-          if self.includes(data):
-              new_node = Node(new_data)
-              current = self.head
-              while (current):
-                  if current.data == data:
-                      new_node.next = current.next
-                      current.next = new_node
-                      break
-                  current = current.next
-        except:
-          raise Exception("the value not found in the list")
-
-def insertBefore(self, data, new_data):
-
-        try:
-            if self.includes(data):
-                new_node = Node(new_data)
-                current = self.head
-                if current.data == data:
-                    new_node.next = self.head
-                    self.head = new_node
-                else:
-                    while (current.next):
-                        if current.next.data == data:
-                            new_node.next = current.next
-                            current.next = new_node
-                            break
-                        current = current.next
-        except:
-          raise Exception("the value not found in the list")
-
-def kthFromEnd(self, k: int):
-
-        length = self.length()
-
-        if k >= length or k < 0:
-            return 'IndexError'
-        current = self.head
-        for i in range(length-k):
-            if i == length-k-1:
-                return current.data
-            current = current.next
-
-def length(self):
-        length = 0
-        current = self.head
-        while(current):
-            length += 1
-            current = current.next
-        return length
-
-        
-def __str__(self):
- 
-       
-        list_data = ""
-      
-        current = self.head
         while current:
-         
-            list_data += f"{current.data} -> "
-           
+            string += f"{str(current.value)} -> "
             current = current.next
-        list_data += "NULL"
+        string += "NULL"
+        return string
+
+    def appendvalue(self,value):
+        new_node=Node(value)
+        if self.head==None:
+            self.head=new_node
+        else:
+            currunt=self.head
+            while currunt.next:
+                currunt=currunt.next
+            currunt.next=new_node
+        
        
-        return list_data
+                
+    def insert_befor(self,value,new_value):
+        new_node=Node(new_value)
+        if self.head.value==value:
+            self.insert(new_value)
+        else:
+            currunt=self.head
+            while currunt.next:
+                if currunt.next.value==value:
+                    new_node.next=currunt.next
+                    currunt.next=new_node
+                    break
+                currunt=currunt.next
 
-        if __name__ == "__main__":
-            
+    def insert_after(self,value,new_value):
+        new_node=Node(new_value)
+        if self.head.value==value:
+            self.head.next=new_node
+        else:
+            currunt=self.head
+            while currunt:
+                if currunt.value==value:
+                    new_node.next=currunt.next
+                    currunt.next=new_node
+                    break
+                currunt=currunt.next   
 
-
-         linked = LinkedList()
-
-
-
-linked.append(1)
-linked.append(3)
-linked.append(2)
-linked.insertAfter(3, 10)
-linked.insertBefore(100, 7)
-linked.insertAfter(25,20)
-print(linked)
+    def kth_from_end(self,num):
+        current=self.head
+        sol=[]
+        if num<0:
+            return 'k is negative, please enter a positive number'
+        
+        counter=0
+        while current.next:
+            counter +=1
+            current=current.next
+        if counter+1==num:
+            return 'the k value is the same as the length of the list, please change it'
+        if counter<num:
+            raise Exception
+        position=counter-num
+        current=self.head
+        for x in range(position):
+            current=current.next
+        return current.value
