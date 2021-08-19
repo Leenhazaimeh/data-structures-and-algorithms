6ddb91c6ecba51c5b79a6b14be40598f96c3d8f2
Leen Hazaimeh <leenhazaimeh94@gmail.com>
2021-08-19 23:46:00 +0300

    finshed code ch 12

--- python/code_challenges/class_12.py/animal_shelter.py
+++ python/code_challenges/class_12.py/animal_shelter.py
@@ -1,30 +1,142 @@
 from new_qeue import *
 
-class AnimalShelter:
-  def __init__(self) :
-      self.cat=Queue()
-      self.dog=Queue()
+class Node():
+ def __init__(self,value=None) -> None:
+       
+        self.value=value
+        self.next=None
 
-  def enqueue(self,animal):
-      if animal=='cat' :
-        self.cat.enqueue(animal)
+
+
+class Stack():
+    def __init__(self,node=None) -> None:
+        self.top=node
+
+    def push(self,value=None):
+        try:
+            node=Node(value)
+            node.next=self.top
+            self.top=node
+        except:
+            raise Exception('Flaaase ')
+
+    def peek(self):
+       
+        try:
+         return self.top.value
+        except:
+         raise Exception('empty Stake')
+    def pop(self):
+        try:
+            temp=self.top
+            self.top=self.top.next
+            temp.next=None
+            return temp.value
+        except:
+            raise Exception(' empty Stake ')
+    
+    def is_empty(self):
+    
+        return not self.top
+
+ 
+
+
+class Queue():
+    def __init__(self) :
+
+       self.rear=None
+       self.front=None
+        
+    
+    def enqueue(self,value):
       
-      elif animal=='dog' :
-        self.dog.enqueue(animal)
+        node=Node(value)
+        if not self.front and not self.rear:
+            self.front=node
+            self.rear=node
 
-      else :
-        return 'not in the list'
-   
+        else:    
+            self.rear.next=node
+            self.rear=node
 
-  def dequeue(self,animal):
-    if animal =='cat' and self.cat.front:
-      return self.cat.dequeue()
+    def dequeue(self):
+        
+        try:
+            temp=self.front
+            self.front=self.front.next
+            temp.next=None
+
+            if self.front==None:
+                 self.rear=None
+
+            return temp.value
+        except:
+            raise Exception("  empty Queue")
+
+    def peek(self):
+       
+        try:
+         return self.front.value
+        except:
+         raise Exception("empty Queue")
+
+    def is_empty(self):
+        
+      return not self.top
+    
+    def __str__(self):
+        string=""
+        while self.front:
+            string+=f"{self.front.value}  "
+            self.front=self.front.next
+        string+="None" 
+        return(string)
+
+    
+class Animal_Shelter():
+    def __init__(self) :
+      self.cats=Queue()
+      self.dogs=Queue()
         
 
-    if animal =='dog'  and self.dog.front:
-       return self.dog.dequeue()
+    def dequeue(self,pref):
 
-    if animal =='dog' or animal =='cat':
-        return 'not there'     
-    else :
-      return 'not in the shelf for ignore it'     
+        if pref=="Dog" or pref=="dog" :
+           return self.dogs.dequeue()
+
+        elif pref=="Cat" or pref=="cat":
+           return self.cats.dequeue()
+
+        else:
+            return None
+   
+    def enqueue(self,animal):
+
+        if animal.type=="Cat":
+            self.cats.enqueue(animal)
+        elif  animal.type=="Dog":
+            self.dogs.enqueue(animal)
+    
+    
+
+            
+            
+class Cat():
+    def __init__(self,name) :
+        self.type="Cat"
+        self.name=name
+
+class Dog():
+    def __init__(self,name) :
+        self.type="Dog"
+        self.name=name
+
+
+
+
+
+# if __name__=="__main__":
+
+#  print ('Hello leen')
+
--- python/code_challenges/class_12.py/test_animal_shelter.py
+++ python/code_challenges/class_12.py/test_animal_shelter.py
@@ -5,31 +5,61 @@ from animal_shelter import *
 
 
 
-def test_AnimalShelter_dequeue(data):
-    assert data.dequeue('cat') == 'cat'
-    assert data.dequeue('cat') == 'cat'
-    assert data.dequeue('dog') == 'dog'
-    assert data.dequeue('dog') == 'dog'
-    assert data.dequeue('dog') == 'not there'
-    assert data.dequeue('cat') == 'not there'
-    assert data.dequeue('bird') == 'not in the shelf for ignore it'
 
-def test_AnimalShelter_enqueue(data):
-    assert data.cat.front.value=='cat'
-    assert data.cat.front.next.value=='cat'
-    assert data.dog.front.value=='dog'
-    assert data.dog.front.next.value=='dog'
-    assert data.enqueue('dd')=='not in the list'
-
-
-
-    
 
 @pytest.fixture
-def data():
-    my_queue = AnimalShelter()
-    my_queue.enqueue('cat')
-    my_queue.enqueue('cat')
-    my_queue.enqueue('dog')
-    my_queue.enqueue('dog')
-    return my_queue
+def animal():
+
+    shelter=Animal_Shelter()
+
+    boxser=Dog("boxser")
+    kok=Dog('kok')
+    mog=Dog('mog')
+
+    luci=Cat("luci")
+    nani=Cat("nani")
+    rafial=Cat("rafial")
+
+    shelter.enqueue(boxser)
+    shelter.enqueue(kok)
+    shelter.enqueue(mog)
+    shelter.enqueue(luci)
+    shelter.enqueue(nani)
+    shelter.enqueue(rafial)
+
+    return shelter
+
+
+
+def test_shelter():
+    assert Animal_Shelter()
+
+
+def test_enqueue_values(animal):
+   
+
+    actual=animal.dogs.front.value.name
+    actual2=animal.cats.front.value.name
+    expected="boxser"
+    expected2="luci"
+
+    assert actual==expected
+    assert actual2==expected2
+
+def test_dequeue_data(animal):
+   
+
+    actual=animal.dequeue("dog")
+    actual2=animal.dequeue("cat")
+    expected="boxser"
+    expected2="luci"
+
+    assert actual.name==expected
+    assert actual2.name==expected2
+
+
+def test_if_none(animal):
+   
+    actual=animal.dequeue("bird")
+    expected=None
+    assert actual==expected
