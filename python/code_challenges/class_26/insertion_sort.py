0cb19e8f4bf38d25c25237106512b97486c2c621
Leen Hazaimeh <leenhazaimeh94@gmail.coom>
2021-08-30 10:24:42 +0300

    add the test

--- python/code_challenges/class_26/insertion_sort.py
+++ python/code_challenges/class_26/insertion_sort.py
@@ -14,13 +14,13 @@ arr_a= [8,4,23,42,16,15]
 arr_b = [20,18,12,8,5,-2]
 arr_c = [5,12,7,5,5,7]
 arr_d = [2,3,5,7,13,11]
-
-Inser_Sort(arr_a)
-Inser_Sort(arr_b )
-Inser_Sort(arr_c)
-Inser_Sort(arr_d)
-print(arr_a)
-print(arr_b )
-print(arr_c)
-print(arr_d)
+if __name__=="__main__":
+    Inser_Sort(arr_a)
+    Inser_Sort(arr_b )
+    Inser_Sort(arr_c)
+    Inser_Sort(arr_d)
+    print(arr_a)
+    print(arr_b )
+    print(arr_c)
+    print(arr_d)
 
