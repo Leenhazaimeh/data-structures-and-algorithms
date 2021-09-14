77530bbfffc684c8cd01d280c090c965684a37b1
Leen Hazaimeh <leenhazaimeh94@gmail.coom>
2021-09-14 23:36:15 +0300

    done

--- python/code_challenges/class_37 /graph_business.py
+++ python/code_challenges/class_37 /graph_business.py
@@ -1,24 +1,115 @@
-def business_trip(name, list):
+class Vertex:
+    def __init__(self, value):
+        self.value = value
 
-    city = False
-    city_two = False
-    cost = 0
+class Graph:
+    def __init__(self):
+        self.adjacency = {}
 
-    for cGraph in len(list):
-        edges = name._adjacency_list[list[cGraph]]
-        city_two = False
+    def add_node(self ,value):
+        node = Vertex(value)
+        self.adjacency[node] = []
+        return node
 
-        for edge in edges:
-            if list[cGraph + 1] == edge.node:
-                cost += edge.weight
-                city = True
-                city_two = True
+    def add_edge(self, first_vertex, second_vertex, weight=1):
+        check = False
+        for i in self.adjacency.keys():
+            if str(i.value) == str(first_vertex):
+                
+                check = True
+                break
+            else:
+                
+                check = False
+        
+        for i in self.adjacency.keys():
+            if str(i.value) == str(second_vertex):
+                
+                check = True
+                break
+            else:
+                check = False
+       
+        if check:
+            for i in self.adjacency.keys():
+                if str(i.value) == str(first_vertex):
+                
+                    self.adjacency[i].append([weight,second_vertex])
+                    break
+                else:
+                
+                    continue
+            
+        else:
+            return 'one of the vertex is not exist'
 
-    city = city and city_two
-  
-    if not city:
-        cost = 0
-        city = False
-        return f"{city},${cost}"
 
-    return f"{city},${cost}"
+    def bfs(self, start_node):
+        nodes=set([])
+        temp = [start_node]
+        values = []
+
+        while len(temp) >0:
+            front_node = temp.pop(0)
+            nodes.add(front_node)
+
+            for i in self.adjacency.keys():
+                if str(i.value) == str(front_node):
+                    values.append(self.adjacency[i])
+                    break
+
+            if len(values) > 0:
+                for n in values[0]:
+                    if n[1] not in nodes:
+                        temp.append(n[1])
+                        nodes.add(n[1])
+            else:
+                return False
+        return nodes
+
+
+
+    def get_nodes(self):
+        if len(list(self.adjacency.keys())) > 0:
+            start = list(self.adjacency.keys())[0].value
+            return self.bfs(start)
+        else:
+            return 'Graph is empty'
+
+    def get_neighbours(self, node):
+        if len(list(self.adjacency.keys())) > 0:
+
+            for i in self.adjacency.keys():
+                if str(i.value) == node:
+                    return self.adjacency[i]
+            
+            return 'Node does not exist'
+        else:
+            return 'Graph is empty'
+
+    def size(self):
+        if len(list(self.adjacency.keys())) > 0:
+            start = list(self.adjacency.keys())[0].value
+            return len(self.bfs(start))
+        else:
+            return 'Graph is empty'
+
+if __name__=='__main__':
+    teast = Graph()
+    teast.add_node('a')
+    teast.add_node('b')
+    teast.add_node('c')
+    teast.add_node('d')
+    teast.add_node('x')
+    teast.add_edge('a','b',8)
+    teast.add_edge('a','d',2)
+    teast.add_edge('a','c')
+    teast.add_edge('b','a',8)
+    teast.add_edge('c','a')
+    teast.add_edge('c','d',7)
+    teast.add_edge('d','a',2)
+    teast.add_edge('d','b',8)
+    teast.add_edge('d','c',7)
+    print(teast.get_nodes())
+    print(teast.get_neighbours('a'))
+    print(teast.bfs('a'))
---
+++ python/code_challenges/class_38/Readme.md
@@ -0,0 +1,24 @@
+# Challenge Summary
+Conduct a depth first preorder traversal on a graph
+Feature Tasks
+
+Write the following method for the Graph class:
+
+    depth first
+    Arguments: Node (Starting point of search)
+    Return: A collection of nodes in their pre-order depth-first traversal order
+    Display the collection
+
+
+## Whiteboard Process
+![Challenge 37 - graph-business-trip](Untitled(10).jpg)
+
+## Approach & Efficiency
+
+    time: O(n)
+    space: O(n)
+
+
+## Solution
+<!-- Show how to run your code, and examples of it in action -->
+
